d04f67398c85259c19620152d6ede8de6d7233ff
gabryelfhsoares <gabryelfhsoares@gmail.com>
2020-09-23 17:48:29 -0300

    Add Welcome.stories

---
+++ 3-Fundamentos/autoescola-nardini/src/stories/0-Welcome.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default {
+    title: "Introduction/Welcome",
+};
+
+export const toAutoEscolaNardini = () => (
+    <div>
+        <h1>Auto Escola Nardini</h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptatibus facilis consectetur doloremque nihil minima repellendus possimus harum, laudantium numquam perferendis officia doloribus repudiandae dignissimos enim tenetur natus totam itaque.
+        </p>
+    </div>
+);
