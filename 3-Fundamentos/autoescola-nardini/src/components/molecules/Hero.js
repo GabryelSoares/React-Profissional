a7fc0ad2b749ebe745910d1a4db17f2831ed1ae7
gabryelfhsoares <gabryelfhsoares@gmail.com>
2020-09-25 10:30:50 -0300

    Add Home Page

--- 3-Fundamentos/autoescola-nardini/src/components/molecules/Hero.js
+++ 3-Fundamentos/autoescola-nardini/src/components/molecules/Hero.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
-import { breakAt, BreakpointSizes } from "../../styles/Breakpoints";
+
+import Container from 'components/atoms/Container';
 
 const Root = styled.div`
     color: #fff;
@@ -16,6 +17,8 @@ const Root = styled.div`
 `;
 
 const Content = styled.div`
+    display: inline-block;
+
     p, 
     li {
         font-size: 20px;
@@ -32,21 +35,6 @@ const Content = styled.div`
             content: "\\2713\\0020";
             color: ${(props) => props.theme.colors.primary.main};
         }
-    }
-`;
-
-const Container = styled.div`
-    width: 100%;
-    padding: 0 8px;
-
-    ${breakAt(BreakpointSizes.sm)} {
-        padding: 0 16px;
-    }
-
-    ${breakAt(BreakpointSizes.lg)} {
-        max-width: 1140px;
-        padding: 0;
-        margin: 0 auto;
     }
 `;
 
